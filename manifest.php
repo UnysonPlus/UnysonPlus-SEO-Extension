7e67c4507931fe5afe87ae0cc38c15327aa84a0b
jonmlas <jonmlas@gmail.com>
2025-08-21 19:02:15 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,21 +1,32 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
+// Basic Info
 $manifest['name']        = __( 'SEO', 'fw' );
+$manifest['slug']        = 'unysonplus-seo';
 $manifest['description'] = __(
-	'This extension will enable you to have a fully optimized WordPress website'
-	.' by adding optimized meta titles, keywords and descriptions.',
-	'fw'
+    'This extension will enable you to have a fully optimized WordPress website by adding optimized meta titles, keywords, and descriptions.',
+    'fw'
 );
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-SEO-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/seo/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
-$manifest['version'] = '1.0.11';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-SEO-Extension';
+$manifest['version']     = '1.0.11';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-SEO-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-SEO-Extension';
+$manifest['github_branch'] = 'master';
+$manifest['uri']           = 'http://manual.unyson.io/en/latest/extension/seo/index.html#content';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
